eddef13c3c6059b46ceede095af29675c95f0488
zhifeng ou <zhifengou@hotmail.com>
2018-11-24 16:46:17 +0000

    automatically backed up by learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,47 @@
 // Code YouTubeDebugger Component Here
+import React,{Component} from 'react';
+
+export default class YouTubeDebugger extends Component {
+
+    state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+        }
+      }
+    }
+
+  handleChangeBitrate = () => {
+    this.setState({
+      settings: {...this.state.settings,bitrate: 12}
+      })
+  }
+
+  handleChangeResolution = () => {
+    this.setState({
+      settings:{...this.state.settings,video:{resolution: '720p'}}
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <button
+          className="bitrate"
+          onClick={this.handleChangeBitrate}
+        >
+          Change bitrate
+        </button>
+        <button
+          className="resolution"
+          onClick={this.handleChangeResolution}
+        >
+          Change resolution
+        </button>
+      </div>
+    );
+  }
+}
